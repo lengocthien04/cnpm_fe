8f15e9d32a85e048c4e3fc3712245f9b79319c75
tianthecore <thienln2004@gmail.com>
2024-11-20 23:55:59 +0700

    fix icon in after login

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 import { AppContext } from "../../contexts/app.context";
 import BeforeLogin from "./children/BeforeLogin/BeforeLogin";
-import AfterLogin from "./children/AfterLogin/AfterLogin"
+import AfterLogin from "./children/AfterLogin/AfterLogin";
 
 export default function HomePage() {
-  const {isAuthenticated}= useContext(AppContext)
-  return <div>{!isAuthenticated ? (<BeforeLogin />) : (<AfterLogin />)}</div>;
+  const { isAuthenticated } = useContext(AppContext);
+  return <div>{isAuthenticated ? <BeforeLogin /> : <AfterLogin />}</div>;
 }
