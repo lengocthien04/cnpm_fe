92605b976471138f020c3cb6eec92c53ce950024
lam nguyen <anhlamnguyen2004@gmail.com>
2024-11-28 13:36:28 +0700

    add userPage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,9 +3,11 @@ import { AppContext } from "../../contexts/app.context";
 import BeforeLogin from "./children/BeforeLogin/BeforeLogin";
 import AfterLogin from "./children/AfterLogin/AfterLogin";
 import PrintingConfig from "../../components/PrintingConfig/PrintingConfig";
+import UserPage from "../UserPage/UserPage";
 
 export default function HomePage() {
   const { isAuthenticated } = useContext(AppContext);
   // return <div>{isAuthenticated ? <BeforeLogin /> : <AfterLogin />}</div>;
-  return <PrintingConfig />
+  // return <PrintingConfig />
+  return <UserPage />
 }
